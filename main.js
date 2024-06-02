aa5d5c4765128f44485d04dce07b0df666e72314
mscervino <mscervino@gmail.com>
2024-06-02 09:22:31 -0300

    ejercicio 2 ok

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ import "./style.css";
 
 //1. Declaración y Asignación: Declara una variable llamada nombre y asígnale tu nombre como valor.
 
+/*
 const name = {
   nombre: "Marcelo",
 };
-console.log(name);
+console.log(name); 
+*/
+
+//2. Tipos de Datos: Crea variables para almacenar un número entero (edad), un número decimal (precio), una cadena de texto (ciudad) y un valor booleano(esEstudiante).
+
+let edad = 59;
+let precio = 1545.99;
+let localidad = "Capital Federal";
+let esEstudiante = true;
+console.log("edad:", edad);
+console.log("precio:", precio);
+console.log("localidad:", localidad);
+console.log("es Estudiante:", esEstudiante);
