a8eb6538dea703c27da729ef5fcc69b3bb7b4835
mscervino <mscervino@gmail.com>
2024-06-02 10:13:35 -0300

    desafio 7 ejercicios 5 - 6 - 7ÇOK

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ console.log(mensaje);
 
 //4. Operaciones Matemáticas: Declara dos variables numéricas (num1, num2) y realiza las operaciones básicas (suma, resta, multiplicación, división) con ellas.
 
+/*
 const numero1 = 24;
 const numero2 = 6;
 
@@ -54,3 +55,35 @@ console.log(
 
 const division = numero1 / numero2;
 console.log("La división de", numero1, "y", numero2, "es:", division);
+*/
+
+//5. Incremento y Decremento: Declara una variable (contador) y utiliza los operadores de incremento (++) y decremento (--) para modificar su valor.
+
+/*
+let contador = 3;
+console.log("Valor inicial del contador:", contador);
+contador++;
+console.log("Después del incremento (contador++):", contador);
+contador++;
+console.log("Después del segundo incremento (contador++):", contador);
+contador--;
+console.log("Después del decremento (contador--):", contador);
+contador--;
+console.log("Después del segundo decremento (contador--):", contador);
+*/
+
+//6. Prompt y Alert: Pide al usuario que ingrese su nombre mediante prompt() y luego muestra un mensaje de bienvenida con alert().
+
+/*
+var nombreUsuario = prompt("Por favor, ingresa tu nombre:");
+alert("¡Bienvenido a IT EDUCATION, " + nombreUsuario + "!");
+*/
+
+//7. Cálculo de Área: Pide al usuario el radio de un círculo y calcula su área (π * radio^2).
+
+/*
+var radio = prompt("Por favor, ingresa el radio del círculo:");
+radio = parseFloat(radio);
+var area = Math.PI * Math.pow(radio, 2);
+alert("El área del círculo con radio " + radio + " es: " + area);
+*/
