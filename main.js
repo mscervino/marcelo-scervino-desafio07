cd847d6b9d3b31904c777cea107b60adfc0b96eb
mscervino <mscervino@gmail.com>
2024-06-02 09:50:38 -0300

    Desafio 7 ejercicios 3 y 4 ok

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ console.log(name);
 
 //2. Tipos de Datos: Crea variables para almacenar un número entero (edad), un número decimal (precio), una cadena de texto (ciudad) y un valor booleano(esEstudiante).
 
+/*
 let edad = 59;
 let precio = 1545.99;
 let localidad = "Capital Federal";
@@ -19,3 +20,37 @@ console.log("edad:", edad);
 console.log("precio:", precio);
 console.log("localidad:", localidad);
 console.log("es Estudiante:", esEstudiante);
+*/
+
+//3. Concatenación: Declara dos variables (nombre, apellido) y muestra un mensaje que diga "Hola, [nombre] [apellido]".
+
+/*
+let nombre = "Marcelo Gustavo";
+let apellido = "Scervino";
+let mensaje = "Hola, " + nombre + " " + apellido;
+console.log(mensaje);
+*/
+
+//4. Operaciones Matemáticas: Declara dos variables numéricas (num1, num2) y realiza las operaciones básicas (suma, resta, multiplicación, división) con ellas.
+
+const numero1 = 24;
+const numero2 = 6;
+
+const suma = numero1 + numero2;
+console.log("La suma de", numero1, "y", numero2, "es:", suma);
+
+const resta = numero1 - numero2;
+console.log("La resta de", numero1, "y", numero2, "es:", resta);
+
+const multiplicacion = numero1 * numero2;
+console.log(
+  "La multiplicación de",
+  numero1,
+  "y",
+  numero2,
+  "es:",
+  multiplicacion
+);
+
+const division = numero1 / numero2;
+console.log("La división de", numero1, "y", numero2, "es:", division);
