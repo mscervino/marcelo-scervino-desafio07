5437afc0a61322e3ffdb4e69e8a81ec25e98afb2
mscervino <mscervino@gmail.com>
2024-06-02 09:13:45 -0300

    ejercicio JS 1 hecho

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import "./style.css";
+
+//1. Declaración y Asignación: Declara una variable llamada nombre y asígnale tu nombre como valor.
+
+const name = {
+  nombre: "Marcelo",
+};
+console.log(name);
